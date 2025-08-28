503ceaa38e87cce1c57e754f37af8d631dddc5f6
sachinacharyaa <sachinacharyahancy@gmail.com>
2025-08-29 00:04:25 +0545

    done

--- src/q12.c
+++ src/q12.c
@@ -1,23 +1,11 @@
-
 #include <stdio.h>
-
 int main()
 {
-
-    char a, b, c;
-    char greater;
-
-    printf("Enter char a value:");
-    scanf(" %c", &a);
-
-    printf("Enter char b value:");
-    scanf(" %c", &b);
-
-    printf("Enter char c value:");
-    scanf(" %c", &c);
-
-    greater = (a > b) && (c != 0);
-    printf("%d\n", greater);
-
-    return 0;
+    int a, b, c, result1, result2;
+    printf("enter a, b, c:");
+    scanf("%d %d %d", &a, &b, &c);
+    result1 = (a > b) && (a > c);
+    printf("%d", result1);
+    result2 = (c != 0);
+    printf("\n%d", result2);
 }
