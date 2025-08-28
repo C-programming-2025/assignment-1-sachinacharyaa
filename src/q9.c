91d260adf59b8aac0573bf6b9bcfbb6b0a61bec8
sachinacharyaa <sachinacharyahancy@gmail.com>
2025-08-28 23:57:10 +0545

    final

--- src/q9.c
+++ src/q9.c
@@ -1,17 +1,11 @@
-
 #include <stdio.h>
-
 int main()
 {
-    int num, weeks, remaining_days;
-
-    printf("Enter a number of days: ");
-    scanf("%d", &num);
-
-    weeks = num / 7;
-    remaining_days = num % 7;
-
-    printf("%d week(s) and %d day(s)\n", weeks, remaining_days);
-
+    int totaldays, weeks, days;
+    printf("enter the number of days:");
+    scanf("%d", &totaldays);
+    weeks = totaldays / 7;
+    days = totaldays % 7;
+    printf("%d days is equvalent to %d weeks and %d days.\n ", totaldays, weeks, days);
     return 0;
 }
