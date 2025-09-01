aca471d03d1f7c11e62d84e80aa85f5e0c680134
sachinacharyaa <sachinacharyahancy@gmail.com>
2025-09-01 11:09:32 +0545

    c

--- src/q13.c
+++ src/q13.c
@@ -7,7 +7,7 @@ int main()
     int num, result;
 
     printf("Enter char a value:");
-    scanf(" %c", &num);
+    scanf(" %c", num);
 
     result = ((num / 3 * 3) == num) && ((num / 2 * 2) == num);
     printf("%d\n", result);
